c3bd4cd5b3fdbf7c51bbdfce23cfdcf32b2a62ad
Defolto <37343435+Defolto@users.noreply.github.com>
2020-06-28 10:03:36 +0300

    create

---
+++ main.py
@@ -0,0 +1,19 @@
+import pymongo
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+CONNECTION_URL = "mongodb+srv://admin:admin@cluster0-x7ysx.mongodb.net/test?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_URL)
+db = client.test
+task_database = client["task"]
+task_list_collection = task_database["task_list"]
+
+
+@app.route('/')
+@app.route('/index')
+def main():
+    return jsonify(', '.join([str(item.get('text')) for item in task_list_collection.find()]))
+
+# app.run()
