f733003ecbf5ae75717c987d713b0662dc2cb58b
Defolto <37343435+Defolto@users.noreply.github.com>
2020-06-28 10:35:20 +0300

    update2

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
-# import pymongo
-# from flask import Flask, jsonify
-
-# app = Flask(__name__)
-# app.config['JSON_AS_ASCII'] = False
-
-# CONNECTION_URL = "mongodb+srv://admin:admin@cluster0-x7ysx.mongodb.net/test?retryWrites=true&w=majority"
-# client = pymongo.MongoClient(CONNECTION_URL)
-# db = client.test
-# task_database = client["task"]
-# task_list_collection = task_database["task_list"]
-
-
-# @app.route('/')
-# @app.route('/index')
-# def main():
-#     return jsonify(', '.join([str(item.get('text')) for item in task_list_collection.find()]))
-
-# app.run()
-
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
 
-user = {
-    "name": "Maks",
-    "age": 21
-}
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
 
-@app.route("/")
-@app.route("/index")
-def main():
-    return jsonify(user)
-
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
